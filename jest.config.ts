7cfd2b3c511f117c0385f2e97b41075fb207dc00
srijanc <srijan_am@yahoo.co.in>
2025-05-26 23:50:47 +1000

    Setting up test env with `jest`

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+    moduleFileExtensions: ['ts', 'tsx', 'js'],
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+};
+
+export default config;
