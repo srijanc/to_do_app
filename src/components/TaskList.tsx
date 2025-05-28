cf7f9b7fd4afa93fe65c0f84186e81180aab81f1
srijanc <srijan_am@yahoo.co.in>
2025-05-29 05:36:31 +1000

    Removed unused import

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useTasks } from '../context/TaskContext';
 import TaskItem from './TaskItem';
 
